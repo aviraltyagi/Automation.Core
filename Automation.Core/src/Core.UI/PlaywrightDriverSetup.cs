75e208350090134d31d084a3fa6d50140d95f83b
aviraltyagi <aviral.tyagi2411@gmail.com>
2022-04-01 20:18:20 +0530

    Adding Playwright core functionality

---
+++ Automation.Core/src/Core.UI/PlaywrightDriverSetup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Threading.Tasks;
+
+namespace Core.UI
+{
+    public class PlaywrightDriverSetup
+    {
+        private PlaywrightDriver _playwrightDriver;
+
+        public async Task<PlaywrightDriver> Init(string browserType, bool isHeadless = false)
+        {
+            try
+            {
+                IPlaywright playwright = await Playwright.CreateAsync();
+                var browserTypeLaunchOptions = new BrowserTypeLaunchOptions()
+                {
+                    Headless = isHeadless,
+                    Args = new[]
+                    {
+                        "--start-maximized"
+                    }
+                };
+                IBrowser browser = null;
+                switch (browserType)
+                {
+                    case BrowserType.Chromium:
+                        browser = await playwright.Chromium.LaunchAsync(browserTypeLaunchOptions);
+                        break;
+                    case BrowserType.Firefox:
+                        browser = await playwright.Firefox.LaunchAsync(browserTypeLaunchOptions);
+                        break;
+                    case BrowserType.Webkit:
+                        browser = await playwright.Webkit.LaunchAsync(browserTypeLaunchOptions);
+                        break;
+                }
+
+                IBrowserContext browserContext = await browser.NewContextAsync(new BrowserNewContextOptions
+                {
+                    IgnoreHTTPSErrors = true,
+                    ViewportSize = ViewportSize.NoViewport
+                });
+
+                IPage page = await browserContext.NewPageAsync();
+
+                _playwrightDriver = new PlaywrightDriver()
+                {
+                    Playwright = playwright,
+                    Browser = browser,
+                    BrowserContext = browserContext,
+                    Page = page
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return _playwrightDriver;
+        }
+    }
+}
---
+++ Automation.Core/test/Api.Test/Steps/PlaywrightBasicSteps.cs
@@ -0,0 +1,42 @@
+﻿using Core.UI;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Api.Test.Steps
+{
+    [Binding]
+    public class PlaywrightBasicSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private PlaywrightDriverSetup _playwrightDriverSetup;
+        private PlaywrightDriver _playwrightDriver;
+
+        public PlaywrightBasicSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"User open '(.*)' browser")]
+        public async Task GivenUserOpenBrowser(string browserType)
+        {
+            _playwrightDriverSetup = new PlaywrightDriverSetup();
+            _playwrightDriver = await _playwrightDriverSetup.Init(browserType);
+            _scenarioContext.Add("PlaywrightDriver", _playwrightDriver);
+        }
+
+        [When(@"I navigate to '(.*)'")]
+        public async Task WhenINavigateTo(string url)
+        {
+            await _playwrightDriver.Page.GotoAsync(url);
+        }
+
+        [Then(@"'(.*)' opens")]
+        public async Task ThenOpens(string url)
+        {
+            string actualUrl = _playwrightDriver.Page.Url;
+            Assert.That(actualUrl.Equals(url), $"Expected Url : {url} but was {actualUrl}");
+        }
+
+    }
+}
